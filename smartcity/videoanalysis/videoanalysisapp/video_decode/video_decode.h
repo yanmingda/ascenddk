8d6a36391a2d8ab381f0b9446323692f2f86df6c
zhangzhaolong <45676440+zhangzhaolong@users.noreply.github.com>
2019-02-21 15:55:50 +0800

    Update video_decode.h
    修改视频结构化解析时两个channel同时解析一个MP4文件或者RTSP时，presenter只能显示一个channel结果的问题

--- smartcity/videoanalysis/videoanalysisapp/video_decode/video_decode.h
+++ smartcity/videoanalysis/videoanalysisapp/video_decode/video_decode.h
@@ -199,4 +199,102 @@ HIAI_DEFINE_PROCESS(INPUT_SIZE, OUTPUT_SIZE)
                       VideoType &video_type);
 
   /**
-   * @brief check t
+   * @brief check the input string is empty or not
+   * @param [in] input_str: the input string
+   * @param [in] channel_id: channel id
+   * @return true: input string is empty; false: input string is not empty
+   */
+  bool IsEmpty(const std::string &input_str, const std::string &channel_id);
+
+  /**
+   * @brief verify the video source name is valid or not
+   * @param [in] input_str: the input string
+   * @return true: input string is valid; false: input string is invalid
+   */
+  bool VerifyVideoSourceName(const std::string &input_str);
+
+  /**
+   * @brief verify the input rtsp is valid or not
+   * @param [in] input_str: the input string
+   * @return true: input rtsp is valid; false: input rtsp is invalid
+   */
+  bool IsValidRtsp(const std::string &input_str);
+
+  /**
+   * @brief verify the input rtsp is valid or not
+   * @param [in] input_str: the input string
+   * @return true: input rtsp is valid; false: input rtsp is invalid
+   */
+  bool IsValidMp4File(const std::string &input_str);
+
+  /**
+   * @brief initialize video stream filter
+   * @param [in] video_type: video type
+   * @param [in] video_filter: video filter
+   */
+  void InitVideoStreamFilter(VideoType video_type,
+                             const AVBitStreamFilter* &video_filter);
+
+  /**
+   * @brief sned finished data to next engine
+   */
+  void SendFinishedData();
+
+  /**
+   * @brief get channel id(integer value)
+   * @param [in] channel_id: the input channel id
+   * @return channel id(integer value)
+   */
+  int GetIntChannelId(const std::string channel_id);
+
+  /**
+   * @brief get channel value
+   * @param [in] channel_id: the input channel id
+   * @return channel value
+   */
+  const std::string &GetChannelValue(const std::string &channel_id);
+
+  /**
+   * @brief ffmpeg post process after get all frame from video
+   * @param [in] av_packet: video packet
+   * @param [in] bsf_ctx: video bsf context
+   */
+  void FfmpegPostProcess(AVPacket &av_packet, AVBSFContext* bsf_ctx);
+
+  /**
+   * @brief set dictionary for rtsp
+   * @param [in] channel_value: the input channel value
+   * @param [in] avdic: video dictionary
+   */
+  void SetDictForRtsp(const std::string &channel_value, AVDictionary* &avdic);
+
+  /**
+   * @brief open video format from input channel
+   * @param [in] channel_value: the input channel value
+   * @param [out] av_format_context: the video format context
+   * @return true: success to open video; false: fail to open video
+   */
+  bool OpenVideoFromInputChannel(const std::string &channel_value,
+                                 AVFormatContext* &av_format_context);
+
+  /**
+   * @brief open video format from input channel
+   * @param [in] videoindex: the video index
+   * @param [out] video_type: the input channel value
+   * @param [in] av_format_context: the video format context
+   * @param [out] bsf_ctx: video bsf context
+   * @return true: success to initialize; false: fail to initialize
+   */
+  bool InitVideoParams(int videoindex, VideoType &video_type,
+                       AVFormatContext* av_format_context,
+                       AVBSFContext* &bsf_ctx);
+
+  /**
+   * @brief send image data by channel
+   * @param [out] current_queue:current channel queue
+   */
+  void SendImageDataByChannel(
+      ThreadSafeQueue<shared_ptr<VideoImageParaT>> &current_queue);
+};
+
+#endif /* VIDEO_DECODE_H_ */
